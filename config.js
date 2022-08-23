67c3c869e314fa1178766165ba525fb0f8749595
Casper Benjamin Norspang <cbn@netic.dk>
2022-08-23 11:17:29 +0200

    Added Weather

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '57.047218',
+	defaultLongitude: '9.920100',
 
 	// Autochange
 	autoChangeTheme: true,
